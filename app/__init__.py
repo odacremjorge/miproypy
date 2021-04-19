93cc2cf94733add2c7ec633d98a8a4d108c7b94d
Jorge Mercado <jorgemercado212@gmail.com>
2021-04-19 12:45:22 -0400

    modelo vista controlador BdD MySQL conectado

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,20 @@
 __version__ = "0.1"
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask('app')
 app.config['SECRET_KEY'] = 'random'
 
+app = Flask(__name__, template_folder="views")
+
+#configuraciones base de datos
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root@localhost/dbmiproypy'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
+
+db = SQLAlchemy(app)
+
+
 app.debug = True
 
-@app.route('/')
-def main():
-    return "hola JR"
+from app.routes.home_router import home_router
+app.register_blueprint(home_router)
